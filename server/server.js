018825428a7421e802fb6b048ed504716ba9ca44
Adrian294@-AGS <stgoadrian294@gmail.com>
2026-04-22 18:45:02 +0800

    on process

--- server/server.js
+++ server/server.js
@@ -8,6 +8,9 @@ const port = process.env.PORT;
 app.use(express.json());
 app.use(express.static('upload'));
 
+app.use("/", authRoute);
+app.use("/admin", adminAuthRoute);
+app.use("/truck", truckRoute);
 
 app.listen(port, () => {
     console.log("server running http://localhost:5000");
